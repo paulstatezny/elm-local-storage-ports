55b517e9e610469a89ae8549d1fdc8505832f4c1
Paul Statezny <paulstatezny@gmail.com>
2017-02-11 20:21:56 -0700

    Utilize Jest/Jasmine better

--- test/local-storage-ports.test.js
+++ test/local-storage-ports.test.js
@@ -21,10 +21,6 @@ function mockReturn(value) {
   return jest.fn(() => value);
 }
 
-function mockCall(mockFn, callNumber) {
-  return mockFn.mock.calls[callNumber || 0];
-}
-
 beforeEach(() => {
   mockPorts = {
     storageGetItem: {subscribe: jest.fn()},
@@ -62,8 +58,7 @@ describe('local-storage-ports', () => {
     test('is a passthrough to `window.localStorage.removeItem`', () => {
       port(mockPorts.storageRemoveItem)('anotherKey');
 
-      expect(mockCall(mockLocalStorage.removeItem))
-        .toEqual(['anotherKey']);
+      expect(mockLocalStorage.removeItem).toHaveBeenCalledWith('anotherKey');
     });
   });
 
@@ -71,8 +66,7 @@ describe('local-storage-ports', () => {
     test('is a passthrough to `window.localStorage.clear`', () => {
       port(mockPorts.storageClear)();
 
-      expect(mockCall(mockLocalStorage.clear))
-        .toEqual([]); // Empty array corresponds to a call without args
+      expect(mockLocalStorage.clear).toHaveBeenCalled();
     });
   });
 
@@ -81,24 +75,21 @@ describe('local-storage-ports', () => {
       mockLocalStorage.getItem = mockReturn('["one", "two"]');
       port(mockPorts.storagePushToSet)(['someSet', 'three']);
 
-      expect(mockCall(mockLocalStorage.setItem))
-        .toEqual(['someSet', '["one","two","three"]']);
+      expect(mockLocalStorage.setItem).toHaveBeenCalledWith('someSet', '["one","two","three"]');
     });
 
     test('does not push a primitive to the array if it already exists in the array', () => {
       mockLocalStorage.getItem = mockReturn('["one", "two"]');
       port(mockPorts.storagePushToSet)(['anotherSet', 'one']);
 
-      expect(mockCall(mockLocalStorage.setItem))
-        .toEqual(['anotherSet', '["one","two"]']);
+      expect(mockLocalStorage.setItem).toHaveBeenCalledWith('anotherSet', '["one","two"]');
     });
 
     test('overwrites the value in localstorage with a new array if it is not an array', () => {
       mockLocalStorage.getItem = mockReturn('{"some": "object"}');
       port(mockPorts.storagePushToSet)(['someKey', 123]);
 
-      expect(mockCall(mockLocalStorage.setItem))
-        .toEqual(['someKey', '[123]']);
+      expect(mockLocalStorage.setItem).toHaveBeenCalledWith('someKey', '[123]');
     });
   });
 
@@ -111,44 +102,38 @@ describe('local-storage-ports', () => {
     test('updates localStorage with the original set if it does not contain the value being removed', () => {
       port(mockPorts.storageRemoveFromSet)(['aSet', "not-in-set"]);
 
-      expect(mockCall(mockLocalStorage.setItem))
-        .toEqual(['aSet', '["one",2,{"num":3},[4],true]']);
+      expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '["one",2,{"num":3},[4],true]');
     });
 
     describe('removes items that yield the same JSON as the item removed', () => {
       test('string', () => {
         port(mockPorts.storageRemoveFromSet)(['aSet', 'one']);
 
-        expect(mockCall(mockLocalStorage.setItem))
-          .toEqual(['aSet', '[2,{"num":3},[4],true]']);
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '[2,{"num":3},[4],true]');
       });
 
       test('number', () => {
         port(mockPorts.storageRemoveFromSet)(['aSet', 2]);
 
-        expect(mockCall(mockLocalStorage.setItem))
-          .toEqual(['aSet', '["one",{"num":3},[4],true]']);
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '["one",{"num":3},[4],true]');
       });
 
       test('object', () => {
         port(mockPorts.storageRemoveFromSet)(['aSet', {num: 3}]);
 
-        expect(mockCall(mockLocalStorage.setItem))
-          .toEqual(['aSet', '["one",2,[4],true]']);
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '["one",2,[4],true]');
       });
 
       test('array', () => {
         port(mockPorts.storageRemoveFromSet)(['aSet', [4]]);
 
-        expect(mockCall(mockLocalStorage.setItem))
-          .toEqual(['aSet', '["one",2,{"num":3},true]']);
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '["one",2,{"num":3},true]');
       });
 
       test('boolean', () => {
         port(mockPorts.storageRemoveFromSet)(['aSet', true]);
 
-        expect(mockCall(mockLocalStorage.setItem))
-          .toEqual(['aSet', '["one",2,{"num":3},[4]]']);
+        expect(mockLocalStorage.setItem).toHaveBeenCalledWith('aSet', '["one",2,{"num":3},[4]]');
       });
     });
   });
